ea80b8a9f12ce5b661264802483c88a1cb2f5fda
sharmaasahill <i.sahilkrsharma@gmail.com>
2026-02-07 09:59:38 +0530

    Update

--- backend/src/prisma/prisma.service.ts
+++ backend/src/prisma/prisma.service.ts
@@ -1,21 +1,5 @@
-import { Injectable, OnModuleInit } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
 
 @Injectable()
-export class PrismaService extends PrismaClient implements OnModuleInit {
-  constructor() {
-    super({
-      datasources: {
-        db: {
-          url: process.env.DATABASE_URL || 'postgresql://dummy:dummy@localhost:5432/dummy',
-        },
-      },
-    });
-  }
-
-  async onModuleInit() {
-    if (process.env.DATABASE_URL) {
-      await this.$connect();
-    }
-  }
-}
+export class PrismaService extends PrismaClient {}
